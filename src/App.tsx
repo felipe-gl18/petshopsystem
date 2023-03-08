a40dd065982d79b0b53cec380876e3d9f23e8416
felipe-gl18 <felipegadelha2004@gmail.com>
2023-03-08 18:23:54 -0300

    fixing error

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,9 @@
-import { useState, createContext, useContext, useEffect } from "react";
-import {
-  Cat,
-  Users,
-  MagnifyingGlass,
-  Plus,
-  Pen,
-  ArrowCircleRight,
-  ArrowCircleDown,
-} from "phosphor-react";
-
-import { gapi } from "gapi-script";
-
 import { NavigationProvider } from "./contexts/nav-context";
 import { MainComponent } from "./components/main-component";
 import { ClientProvider } from "./contexts/clients-context";
 import { PetProvider } from "./contexts/pet-context";
 
 function App() {
-  const [documents, setDocuments] = useState();
-  const [signedInUser, setSignedInUser] = useState();
-  const [isLoadingGoogleDriveApi, setIsLoadingGoogleDriveApi] =
-    useState<boolean>();
-  const [isFetchingGoogleDriveFiles, setIsFetchingGoogleDriveFiles] =
-    useState<boolean>();
-
-  const [listDocumentsVisibility, setListDocumentsVisibility] =
-    useState<boolean>();
-
-  const API_KEY = "AIzaSyDqxmzYfCBtHR2EEK45f-sMtzkAa0e6xfY";
-  const CLIENT_ID =
-    "285393898991-r81lciffuskvkasb7gackrv946pc8dho.apps.googleusercontent.com";
-  const DISCOVERY_DOC =
-    "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest";
-  const SCOPES = "https://www.googleapis.com/auth/drive.metadata.readonly";
-
-  function gapiLoaded() {
-    gapi.load("client", initializeGapiClient);
-  }
-
-  async function initializeGapiClient() {
-    await gapi.client
-      .init({
-        apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC],
-      })
-      .then(
-        function () {
-          // Listen for sign-in state changes.
-          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-          // Handle the initial sign-in state.
-          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        },
-        function (error: any) {}
-      );
-  }
-  const updateSigninStatus = (isSignedIn: any) => {
-    if (isSignedIn) {
-      // Set the signed in user
-      setSignedInUser(gapi.auth2.getAuthInstance().currentUser.je.Qt);
-      setIsLoadingGoogleDriveApi(false);
-      // list files if user is authenticated
-      listFiles();
-    } else {
-      // prompt user to sign in
-      handleAuthClick();
-    }
-  };
-
-  function handleAuthClick() {
-    gapi.auth2.getAuthInstance().signIn();
-  }
-
-  async function listFiles(searchTerm?: any) {
-    setIsFetchingGoogleDriveFiles(true);
-    gapi.client.drive.files
-      .list({
-        pageSize: 10,
-        fields: "nextPageToken, files(id, name, mimeType, modifiedTime)",
-        q: searchTerm,
-      })
-      .then(function (response: any) {
-        setIsFetchingGoogleDriveFiles(false);
-        setListDocumentsVisibility(true);
-        const res = JSON.parse(response.body);
-        setDocuments(res.files);
-      });
-  }
   return (
     <NavigationProvider>
       <ClientProvider>
@@ -94,7 +11,6 @@ function App() {
           <MainComponent />
         </PetProvider>
       </ClientProvider>
-      <button onClick={gapiLoaded}>seila</button>
     </NavigationProvider>
   );
 }
