fc4e8dd9152422b7e958422a8d31536dca12af7d
felipe-gl18 <felipegadelha2004@gmail.com>
2023-03-12 14:34:11 -0300

    fixing google drive picker error

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { NavigationProvider } from "./contexts/nav-context";
 import { MainComponent } from "./components/main-component";
 import { ClientProvider } from "./contexts/clients-context";
 import { PetProvider } from "./contexts/pet-context";
-import useDrivePicker from "react-google-drive-picker";
 
 function App() {
-  const [openPicker, data] = useDrivePicker();
-  let dataUrl;
-
   return (
     <NavigationProvider>
       <ClientProvider>
